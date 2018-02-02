4fe130fe39dba14f3d12a726e601a9de08acac8d
Rohit Navalgund <rn@Rohits-MacBook-Air.local>
2018-02-02 18:33:10 +0530

    AppleTV + UI Details.

--- index.php
+++ index.php
@@ -49,7 +49,8 @@
 
 	<!-- 64bit devices -->
 
-	<!-- iPhone 10,1 iPhone 8 -->
+	<!-- iPhone 10,1 iPhone 8 (Global) -->
+	<br> iPhone 8 (Global)
 	<a href="check.php?device=iPhone10,1&boardconfig=d20ap&version=11.2.5">
 	<div class="btn">
 	<center>
@@ -57,15 +58,53 @@
 	</center>
 	</div>
 
-	<!-- iPhone 10,4 iPhone 8 -->
-	<a href="check.php?device=iPhone10,1&boardconfig=d201ap&version=11.2.5">
+	<!-- iPhone 10,4 iPhone 8 (GSM) -->
+	<br> iPhone 8 (GSM)
+	<a href="check.php?device=iPhone10,4&boardconfig=d201ap&version=11.2.5">
 	<div class="btn">
 	<center>
 	iPhone10,4, iOS 11.2.5
 	</center>
 	</div>
+
+	<!-- iPhone 10,2 iPhone 8 plus (Global) -->
+	<br> iPhone 8 Plus (Global)
+	<a href="check.php?device=iPhone10,2&boardconfig=d21ap&version=11.2.5">
+	<div class="btn">
+	<center>
+	iPhone10,2, iOS 11.2.5
+	</center>
+	</div>
+
+	<!-- iPhone 10,5 iPhone 8  plus (GSM) -->
+	<br> iPhone 8 Plus (GSM)
+	<a href="check.php?device=iPhone10,5&boardconfig=d211ap&version=11.2.5">
+	<div class="btn">
+	<center>
+	iPhone10,5, iOS 11.2.5
+	</center>
+	</div>
+
+	<!-- iPhone 10,1 iPhone X -->
+	<br> iPhone X (GSM)
+	<a href="check.php?device=iPhone10,3&boardconfig=d22ap&version=11.2.5">
+	<div class="btn">
+	<center>
+	iPhone10,3, iOS 11.2.5
+	</center>
+	</div>
+
+	<!-- iPhone 10,4 iPhone X (Global) -->
+	<br> iPhone X (Global)
+	<a href="check.php?device=iPhone10,6&boardconfig=d221ap&version=11.2.5">
+	<div class="btn">
+	<center>
+	iPhone10,6, iOS 11.2.5
+	</center>
+	</div>
 	
 	<!-- iPhone 8,4 iPhone SE (TSMC) -->
+	<br> iPhone SE (TSMC)
 	<a href="check.php?device=iPhone8,4&boardconfig=n69uap&version=11.2.5">
 	<div class="btn">
 	<center>
@@ -74,6 +113,7 @@
 	</div>
 
 	<!-- iPhone 8,4 iPhone SE (Samsung)-->
+	<br> iPhone SE (Samsung)
 	<a href="check.php?device=iPhone8,4&boardconfig=n69ap&version=11.2.5">
 	<div class="btn">
 	<center>
@@ -81,31 +121,8 @@
 	</center>
 	</div>
 	
-	<!-- iPhone 9,1 iPhone 7 -->
-	<a href="check.php?device=iPhone9,1&boardconfig=d10ap&version=11.2.5">
-	<div class="btn_bottom">
-	<center>
-	iPhone 9,1, iOS 11.2.5 
-	</center>
-	</div>
-	
-	<!-- iPhone 8,1 iPhone 6s (TSMC) -->
-	<a href="check.php?device=iPhone8,1&boardconfig=n71map&version=11.2.5">
-	<div class="btn_bottom">
-	<center>
-	iPhone 8,1, iOS 11.2.5 
-	</center>
-	</div>
-
-	<!-- iPhone 8,1 iPhone 6s (Samsung) -->
-	<a href="check.php?device=iPhone8,1&boardconfig=n71ap&version=11.2.5">
-	<div class="btn_bottom">
-	<center>
-	iPhone 8,1, iOS 11.2.5 
-	</center>
-	</div>
-
-	<!-- iPhone 9,2 iPhone 7 Plus -->
+	<!-- iPhone 9,2 iPhone 7 Plus (Global) -->
+	<br> iPhone 7 Plus (Global)
 	<a href="check.php?device=iPhone9,1&boardconfig=d11ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -113,7 +130,8 @@
 	</center>
 	</div>
 
-	<!-- iPhone 9,4 iPhone 7 Plus -->
+	<!-- iPhone 9,4 iPhone 7 Plus (GSM) -->
+	<br> iPhone 7 Plus (GSM)
 	<a href="check.php?device=iPhone9,4&boardconfig=d111ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -121,7 +139,17 @@
 	</center>
 	</div>
 
-	<!-- iPhone 9,3 iPhone 7 -->
+	<!-- iPhone 9,1 iPhone 7 (Global) -->
+	<br> iPhone 7 (Global)
+	<a href="check.php?device=iPhone9,1&boardconfig=d10ap&version=11.2.5">
+	<div class="btn_bottom">
+	<center>
+	iPhone 9,1, iOS 11.2.5 
+	</center>
+	</div>
+	
+	<!-- iPhone 9,3 iPhone 7 (GSM)-->
+	<br> iPhone 7 (GSM)
 	<a href="check.php?device=iPhone9,3&boardconfig=d101ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -130,6 +158,7 @@
 	</div>
 
 	<!-- iPhone 8,1 iPhone 6s (Samsung) -->
+	<br> iPhone 6s (Samsung)
 	<a href="check.php?device=iPhone8,1&boardconfig=n71ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -138,6 +167,7 @@
 	</div>
 
 	<!-- iPhone 8,2 iPhone 6s  (TSMC) -->
+	<br> iPhone 6s (TSMC)
 	<a href="check.php?device=iPhone8,1&boardconfig=n71map&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -146,6 +176,7 @@
 	</div>
 
 	<!-- iPhone 8,2 iPhone 6s Plus (Samsung) -->
+	<br> iPhone 6s Plus (Samsung)
 	<a href="check.php?device=iPhone8,2&boardconfig=n66ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -154,6 +185,7 @@
 	</div>
 
 	<!-- iPhone 8,2 iPhone 6s Plus (TSMC) -->
+	<br> iPhone 6s Plus (TSMC)
 	<a href="check.php?device=iPhone8,2&boardconfig=n66map&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -162,6 +194,7 @@
 	</div>
 
 	<!-- iPhone 7,1 iPhone 6 Plus -->
+	<br> iPhone 6 Plus
 	<a href="check.php?device=iPhone7,1&boardconfig=n56ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -170,6 +203,7 @@
 	</div>
 
 	<!-- iPhone 7,2 iPhone 6 -->
+	<br> iPhone 6
 	<a href="check.php?device=iPhone7,2&boardconfig=n61ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -178,6 +212,7 @@
 	</div>
 
 	<!-- iPhone 6,1 iPhone 5s (GSM) -->
+	<br> iPhone 5s (GSM)
 	<a href="check.php?device=iPhone6,1&boardconfig=n51ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -186,6 +221,7 @@
 	</div>
 
 	<!-- iPhone 6,2 iPhone 5s (Global) -->
+	<br> iPhone 5s (Global)
 	<a href="check.php?device=iPhone6,2&boardconfig=n53ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -197,7 +233,8 @@
 	<!-- 32BIT DEVICES STARTS HERE -->
 
 
-	<!-- iPhone 5,4 iPhone 5c -->
+	<!-- iPhone 5,4 iPhone 5c (Global) -->
+	<br> iPhone 5c (Global)
 	<a href="check.php?device=iPhone5,4&boardconfig=n49app&version=10.3.3">
 	<div class="btn_bottom">
 	<center>
@@ -205,7 +242,8 @@
 	</center>
 	</div>
 
-	<!-- iPhone 5,3 iPhone 5c -->
+	<!-- iPhone 5,3 iPhone 5c (GSM) -->
+	<br> iPhone 5c (GSm)
 	<a href="check.php?device=iPhone5,3&boardconfig=n48ap&version=10.3.3">
 	<div class="btn_bottom">
 	<center>
@@ -213,7 +251,8 @@
 	</center>
 	</div>
 
-	<!-- iPhone 5,1 iPhone 5 -->
+	<!-- iPhone 5,1 iPhone 5 (GSM) -->
+	<br> iPhone 5 (GSM)
 	<a href="check.php?device=iPhone5,1&boardconfig=n41ap&version=10.3.3">
 	<div class="btn_bottom">
 	<center>
@@ -221,7 +260,8 @@
 	</center>
 	</div>
 
-	<!-- iPhone 5,2 iPhone 5 -->
+	<!-- iPhone 5,2 iPhone 5 (Global) -->
+	<br> iPhone 5 (Global)
 	<a href="check.php?device=iPhone5,2&boardconfig=n42ap&version=10.3.3">
 	<div class="btn_bottom">
 	<center>
@@ -230,6 +270,7 @@
 	</div>
 
 	<!-- iPhone 4,1 iPhone 4s -->
+	<br> iPhone 4s
 	<a href="check.php?device=iPhone4,1&boardconfig=n94ap&version=9.3.5">
 	<div class="btn_bottom">
 	<center>
@@ -238,6 +279,7 @@
 	</div>
 
 	<!-- iPhone 3,3 iPhone 4 (CDMA)-->
+	<br> iPhone 4 (CDMA)
 	<a href="check.php?device=iPhone3,3&boardconfig=n92ap&version=7.1.2">
 	<div class="btn_bottom">
 	<center>
@@ -246,6 +288,7 @@
 	</div>
 
 	<!-- iPhone 3,2 iPhone 4 (GSM / 2012) -->
+	<br> iPhone 4 (GSM / 2012)
 	<a href="check.php?device=iPhone3,2&boardconfig=n90bap&version=7.1.2">
 	<div class="btn_bottom">
 	<center>
@@ -254,6 +297,7 @@
 	</div>
 
 	<!-- iPhone 3,1 iPhone 4 (GSM) -->
+	<br> iPhone 4 (GSM)
 	<a href="check.php?device=iPhone3,1&boardconfig=n90ap&version=7.1.2">
 	<div class="btn_bottom">
 	<center>
@@ -262,6 +306,7 @@
 	</div>
 
 	<!-- iPhone 2,1 iPhone 3GS -->
+	<br> iPhone 3GS 
 	<a href="check.php?device=iPhone2,1&boardconfig=n88ap&version=6.1.6">
 	<div class="btn_bottom">
 	<center>
@@ -270,6 +315,7 @@
 	</div>
 
 	<!-- iPhone 1,2 iPhone 3g -->
+	<br> iPhone 3G
 	<a href="check.php?device=iPhone1,2&boardconfig=n82ap&version=4.2.1">
 	<div class="btn_bottom">
 	<center>
@@ -278,6 +324,7 @@
 	</div>
 
 	<!-- iPhone 1,1 iPhone 2g -->
+	<br> iPhone 2G
 	<a href="check.php?device=iPhone1,1&boardconfig=n42ap&version=3.1.3">
 	<div class="btn_bottom">
 	<center>
@@ -291,6 +338,7 @@
 	<!-- Non-mini devices -->
 
 	<!-- iPad 1,1 iPad 1-->
+	<br> iPad 1st Gen
 	<a href="check.php?device=iPad1,1&boardconfig=k48ap&version=5.1.1">
 	<div class="btn_bottom">
 	<center>
@@ -299,6 +347,7 @@
 	</div>
 
 	<!-- iPad 2,1 iPad 2 (Wifi)-->
+	<br> iPad 2 (Wifi)
 	<a href="check.php?device=iPad2,1&boardconfig=k93ap&version=9.3.5">
 	<div class="btn_bottom">
 	<center>
@@ -307,6 +356,7 @@
 	</div>
 
 	<!-- iPad 2,2 iPad 2 (GSM) -->
+	<br> iPad 2 (GSM)
 	<a href="check.php?device=iPad2,2&boardconfig=k94ap&version=9.3.5">
 	<div class="btn_bottom">
 	<center>
@@ -315,6 +365,7 @@
 	</div>
 
 	<!-- iPad 2,3 iPad 2 (CDMA) -->
+	<br> iPad 2 (CDMA)
 	<a href="check.php?device=iPad2,3&boardconfig=k95ap&version=9.3.5">
 	<div class="btn_bottom">
 	<center>
@@ -323,6 +374,7 @@
 	</div>
 
 	<!-- iPad 2,4 iPad 2 (Mid 2012) -->
+	<br> iPad 2 (Mid 2012)
 	<a href="check.php?device=iPad2,4&boardconfig=k95aap&version=9.3.5">
 	<div class="btn_bottom">
 	<center>
@@ -331,6 +383,7 @@
 	</div>
 
 	<!-- iPad 3,1 iPad 3 (Wifi) -->
+	<br> iPad 3 (Wifi)
 	<a href="check.php?device=iPad3,1&boardconfig=j1ap&version=9.3.5">
 	<div class="btn_bottom">
 	<center>
@@ -339,6 +392,7 @@
 	</div>
 
 	<!-- iPad 3,2 iPad 3 (GSM) -->
+	<br> iPad 3 (GSM)
 	<a href="check.php?device=iPad3,2&boardconfig=j2ap&version=9.3.5">
 	<div class="btn_bottom">
 	<center>
@@ -347,6 +401,7 @@
 	</div>
 
 	<!-- iPad 3,3 iPad 3 (Global) -->
+	<br> iPad 3 (Global)
 	<a href="check.php?device=iPad3,3&boardconfig=j2aap&version=9.3.5">
 	<div class="btn_bottom">
 	<center>
@@ -355,6 +410,7 @@
 	</div>
 
 	<!-- iPad 3,4 iPad 4 (wifi) -->
+	<br> iPad 4 (Wifi)
 	<a href="check.php?device=iPad3,4&boardconfig=p101ap&version=10.3.3">
 	<div class="btn_bottom">
 	<center>
@@ -363,6 +419,7 @@
 	</div>
 
 	<!-- iPad 3,5 iPad 4  (GSM) -->
+	<br> iPad 4 (GSM)
 	<a href="check.php?device=iPad3,5&boardconfig=p102ap&version=10.3.3">
 	<div class="btn_bottom">
 	<center>
@@ -371,6 +428,7 @@
 	</div>
 
 	<!-- iPad 3,6 iPad 4 (Global) -->
+	<br> iPad 4 (Global)
 	<a href="check.php?device=iPad3,6&boardconfig=p103ap&version=10.3.3">
 	<div class="btn_bottom">
 	<center>
@@ -379,6 +437,7 @@
 	</div>
 
 	<!-- iPad 4,1 iPad Air (Wifi) -->
+	<br> iPad Air (Wifi)
 	<a href="check.php?device=iPad4,1&boardconfig=j71ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -387,6 +446,7 @@
 	</div>
 
 	<!-- iPad 4,2 iPad Air (Cellular) -->
+	<br> iPad Air (Cellular)
 	<a href="check.php?device=iPad4,2&boardconfig=j72aap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -395,6 +455,7 @@
 	</div>
 
 	<!-- iPad 4,3 iPad Air (China) -->
+	<br> iPad Air (China)
 	<a href="check.php?device=iPad4,3&boardconfig=j73ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -402,23 +463,26 @@
 	</center>
 	</div>
 
-	<!-- iPad 5,1 iPad Air 2-->
-	<a href="check.php?device=iPad5,1&boardconfig=j81ap&version=11.2.5">
+	<!-- iPad 5,3 iPad Air 2 (Wifi) -->
+	<br> iPad Air 2 (Wifi)
+	<a href="check.php?device=iPad5,3&boardconfig=j81ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
-	iPad 5,1, iOS 11.2.5
+	iPad 5,3, iOS 11.2.5
 	</center>
 	</div>
 
-	<!-- iPad 5,2 iPad Air 2-->
-	<a href="check.php?device=iPad5,2&boardconfig=j82ap&version=11.2.5">
+	<!-- iPad 5,4 iPad Air 2 (Cellular) -->
+	<br> iPad Air 2 (Cellular)
+	<a href="check.php?device=iPad5,4&boardconfig=j82ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
-	iPad 5,2, iOS 11.2.5
+	iPad 5,4, iOS 11.2.5
 	</center>
 	</div>
 
-	<!-- iPad 6,7 iPad Pro (12.9) 1st Gen -->
+	<!-- iPad 6,7 iPad Pro (12.9) 1st Gen (Wifi) -->
+	<br> iPad Pro (12.9 1st Gen / Wifi)
 	<a href="check.php?device=iPad6,7&boardconfig=j98aap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -426,7 +490,8 @@
 	</center>
 	</div>
 
-	<!-- iPad 6,8 iPad Pro (12.9) 1st Gen -->
+	<!-- iPad 6,8 iPad Pro (12.9) 1st Gen (Cellular) -->
+	<br> iPad Pro (12.9 1st Gen / Cellular)
 	<a href="check.php?device=iPad6,8&boardconfig=j99aap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -434,7 +499,8 @@
 	</center>
 	</div>
 
-	<!-- iPad 6,3 iPad Pro (9.7) -->
+	<!-- iPad 6,3 iPad Pro (9.7) (Wifi) -->
+	<br> iPad Pro (9.7 / Wifi)
 	<a href="check.php?device=iPad6,3&boardconfig=j127ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -442,7 +508,8 @@
 	</center>
 	</div>
 
-	<!-- iPad 6,4 iPad Pro (9.7) -->
+	<!-- iPad 6,4 iPad Pro (9.7) (Cellular)-->
+	<br> iPad Pro (9.7 / Cellular)
 	<a href="check.php?device=iPad6,4&boardconfig=j128ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -450,7 +517,8 @@
 	</center>
 	</div>
 
-	<!-- iPad 6,11 iPad 5 -->
+	<!-- iPad 6,11 iPad 5 (wifi) -->
+	<br> iPad 5 (Wifi)
 	<a href="check.php?device=iPad6,11&boardconfig=j71sap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -458,7 +526,8 @@
 	</center>
 	</div>
 
-	<!-- iPad 6,12 iPad 5 -->
+	<!-- iPad 6,12 iPad 5 (Cellular) -->
+	<br> iPad 5 (Cellular)
 	<a href="check.php?device=iPad6,12&boardconfig=j72sap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -467,7 +536,8 @@
 	</div>
 
 
-	<!-- iPad 7,1 iPad Pro (12.9) 2nd Gen -->
+	<!-- iPad 7,1 iPad Pro (12.9) 2nd Gen (Wifi) -->
+	<br> iPad Pro (12.9 2st Gen / Wifi)
 	<a href="check.php?device=iPad7,1&boardconfig=j120ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -475,7 +545,8 @@
 	</center>
 	</div>
 
-	<!-- iPad 7,2 iPad Pro (12.9) 2nd Gen -->
+	<!-- iPad 7,2 iPad Pro (12.9) 2nd Gen (Cellular) -->
+	<br> iPad Pro (12.9 2st Gen / Cellular)
 	<a href="check.php?device=iPad7,2&boardconfig=j121ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -484,6 +555,7 @@
 	</div>
 
 	<!-- iPad 7,3 iPad Pro (10.5) -->
+	<br> iPad Pro (10.5 / Wifi)
 	<a href="check.php?device=iPad7,3&boardconfig=j207ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -492,6 +564,7 @@
 	</div>
 
 	<!-- iPad 7,4 iPad Pro (10.5) -->
+	<br> iPad Pro (10.5 / Cellular)
 	<a href="check.php?device=iPad7,4&boardconfig=j208ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -503,6 +576,7 @@
 	<!-- Mini Models -->
 	
 	<!-- iPad 2,5 iPad Mini (wifi) -->
+	<br> iPad Mini (Wifi)
 	<a href="check.php?device=iPad2,5&boardconfig=p105ap&version=9.3.5">
 	<div class="btn_bottom">
 	<center>
@@ -511,6 +585,7 @@
 	</div>
 	
 	<!-- iPad 2,6 iPad Mini (GSM) -->
+	<br> iPad Mini (GSM)
 	<a href="check.php?device=iPad2,6&boardconfig=p106ap&version=9.3.5">
 	<div class="btn_bottom">
 	<center>
@@ -519,6 +594,7 @@
 	</div>
 	
 	<!-- iPad 2,7 iPad Mini (Global) -->
+	<br> iPad Mini (Global)
 	<a href="check.php?device=iPad2,7&boardconfig=p107ap&version=9.3.5">
 	<div class="btn_bottom">
 	<center>
@@ -526,7 +602,8 @@
 	</center>
 	</div>
 
-	<!-- iPad 4,4 iPad Mini 2 -->
+	<!-- iPad 4,4 iPad Mini 2 (Wifi) -->
+	<br> iPad Mini 2 (Wifi)
 	<a href="check.php?device=iPad4,4&boardconfig=j85ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -534,7 +611,8 @@
 	</center>
 	</div>
 	
-	<!-- iPad 4,5 iPad Mini 2 -->
+	<!-- iPad 4,5 iPad Mini 2 (Cellular) -->
+	<br> iPad Mini 2 (Cellular)
 	<a href="check.php?device=iPad4,5&boardconfig=j86ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -543,6 +621,7 @@
 	</div>
 	
 	<!-- iPad 4,6 iPad Mini 2 -->
+	<br> iPad Mini 2 (China)
 	<a href="check.php?device=iPad4,6&boardconfig=j87ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -550,7 +629,8 @@
 	</center>
 	</div>
 
-		<!-- iPad 4,7 iPad Mini 3 -->
+	<!-- iPad 4,7 iPad Mini 3 -->
+	<br> iPad Mini 3 (Wifi)
 	<a href="check.php?device=iPad4,7&boardconfig=j85map&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -558,7 +638,8 @@
 	</center>
 	</div>
 	
-	<!-- iPad 4,8 iPad Mini 3 -->
+	<!-- iPad 4,8 iPad Mini 3 (Cellular) -->
+	<br> iPad Mini 3 (Cellular)
 	<a href="check.php?device=iPad4,8&boardconfig=j86map&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -566,7 +647,8 @@
 	</center>
 	</div>
 	
-	<!-- iPad 4,9 iPad Mini 3 -->
+	<!-- iPad 4,9 iPad Mini 3 (China) -->
+	<br> iPad Mini 3 (China)
 	<a href="check.php?device=iPad4,8&boardconfig=j87map&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -574,7 +656,8 @@
 	</center>
 	</div>
 
-	<!-- iPad 5,1 iPad Mini 4 -->
+	<!-- iPad 5,1 iPad Mini 4 (Wifi) -->
+	<br> iPad Mini 4 (Wifi)
 	<a href="check.php?device=iPad5,1&boardconfig=j96ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
@@ -582,11 +665,119 @@
 	</center>
 	</div>
 	
-	<!-- iPad 5,2 iPad Mini 4 -->
+	<!-- iPad 5,2 iPad Mini 4 (Cellular) -->
+	<br> iPad Mini 4 (Cellular)
 	<a href="check.php?device=iPad5,2&boardconfig=j97ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
 	iPad 5,2, iOS 11.2.5
+	</center>
+	</div>
+
+
+	<!-- iPod Touch -->
+
+	<!-- iPod 1,1 iPod Touch 1st Gen -->
+	<br> iPod Touch 1G
+	<a href="check.php?device=iPod1,1&boardconfig=n45ap&version=1.1.5">
+	<div class="btn_bottom">
+	<center>
+	iPod1,1, iOS 1.1.5
+	</center>
+	</div>
+
+	<!-- iPod 2,1 iPod Touch 2nd Gen -->
+	<br> iPod Touch 2G
+	<a href="check.php?device=iPod2,1&boardconfig=n72ap&version=4.2.1">
+	<div class="btn_bottom">
+	<center>
+	iPod2,1, iOS 4.2.1
+	</center>
+	</div>
+
+	<!-- iPod 3,1 iPod Touch 3rd Gen -->
+	<br> iPod Touch 3G
+	<a href="check.php?device=iPod3,1&boardconfig=n18ap&version=5.1.1">
+	<div class="btn_bottom">
+	<center>
+	iPod3,1, iOS 5.1.1
+	</center>
+	</div>
+
+	<!-- iPod 4,1 iPod Touch 4th Gen -->
+	<br> iPod Touch 4G
+	<a href="check.php?device=iPod4,1&boardconfig=n81ap&version=6.1.6">
+	<div class="btn_bottom">
+	<center>
+	iPod4,1, iOS 6.1.6
+	</center>
+	</div>
+
+	<!-- iPod 5,1 iPod Touch 5th Gen -->
+	<br> iPod Touch 5G
+	<a href="check.php?device=iPod5,1&boardconfig=n78ap&version=9.3.5">
+	<div class="btn_bottom">
+	<center>
+	iPod5,1, iOS 9.3.5
+	</center>
+	</div>
+
+	<!-- iPod 5,1 iPod Touch 5th Gen <black and silver> -->
+	<br> iPod Touch 5G
+	<a href="check.php?device=iPod5,1&boardconfig=n78aap&version=9.3.5">
+	<div class="btn_bottom">
+	<center>
+	iPod5,1, iOS 9.3.5
+	</center>
+	</div>
+
+	<!-- iPod 7,1 iPod Touch 6th Gen -->
+	<br> iPod Touch 6G
+	<a href="check.php?device=iPod7,1&boardconfig=n102aap&version=11.2.5">
+	<div class="btn_bottom">
+	<center>
+	iPod7,1, iOS 11.2.5
+	</center>
+	</div>
+
+
+
+
+	<!-- APPLE TV -->
+
+	<!-- AppleTV 2,1 AppleTV 2nd Gen -->
+	<br> Apple TV 2G
+	<a href="check.php?device=AppleTV2,1&boardconfig=k66ap&version=7.1.2">
+	<div class="btn_bottom">
+	<center>
+	AppleTV 2,1, iOS 7.1.2
+	</center>
+	</div>
+
+	<!-- AppleTV 3,1 AppleTV 3rd Gen -->
+	<br> Apple TV 3
+	<a href="check.php?device=AppleTV3,1&boardconfig=j33ap&version=8.4.2">
+	<div class="btn_bottom">
+	<center>
+	AppleTV 3,1, iOS 8.4.2
+	</center>
+	</div>
+
+	<!-- AppleTV 3,2 AppleTV 3rd Gen (2013) -->
+	<br> Apple TV 3 (2013)
+	<a href="check.php?device=AppleTV3,2&boardconfig=j33iap&version=8.4.2">
+	<div class="btn_bottom">
+	<center>
+	AppleTV 3,2, iOS 8.4.2
+	</center>
+	</div>
+
+	<!-- AppleTV 5,3 AppleTV 4th Gen -->
+	<br> Apple TV 4
+	<a href="check.php?device=AppleTV5,3&boardconfig=j105aap&version=11.2.5">
+	<div class="btn_bottom">
+	<center>
+	AppleTV 5,3, iOS 11.2.5
 	</center>
 	</div>
 	
