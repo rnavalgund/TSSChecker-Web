2937d19c23ff45a8713f4a0b36c43a5c03ce4a0c
DanielD3V <danieldevofficial@gmail.com>
2018-02-02 10:20:06 +0100

    Cleaner UI

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta name="viewport" content="width=device-width">
+	<meta name = "viewport" content = "width = device-width">
 	<title>TSS Web</title>
 	<style>
-	body {
+	body{
 		margin: 0;
 	}
-
-	.navbar {
+	.navbar{
 		color: white;
 		background-color: black;
 		width: 100%;
@@ -16,29 +16,61 @@
 		font-family: arial;
 		box-shadow: 0px 0px 2px 2px lightgray;
 		padding: 16px 0px;
-
 	}
-	.btn {
+	.btn{
 		width: 100%;
 		background-color: white;
 		text-align: center;
 		color: black;
 		font-family: arial;
-		box-shadow: 0px 0px 2px 2px lightgray;
+		border-top: 1px lightgray solid;
 		padding: 16px 0px;
 	}
-
-	a {
+	.btn_bottom{
+		width: 100%;
+		background-color: white;
+		text-align: center;
+		color: black;
+		font-family: arial;
+		border-top: 1px lightgray solid;
+		border-bottom: 1px lightgray solid;
+		padding: 16px 0px;
+	}
+	a{
 		text-decoration: none;
 		color: black;
 	}
 </style>
 </head>
 <body>
-	<div class="navbar">TSSChecker - Web</div>
+	<div class="navbar">
+	TSSChecker - Web - iPhone
+	</div>
 	<br>
-	<br>
-	<a href="check.php?device=iPhone8,4&boardconfig=n69uap&version=11.2.5"><div class="btn"><center>iPhone8,4, iOS 11.2.5</center></div>
+
+		<!-- iPhone 10,1 -->
+	<a href="check.php?device=iPhone10,1&boardconfig=d20ap&version=11.2.5">
+	<div class="btn">
+	<center>
+	iPhone10,1, iOS 11.2.5
+	</center>
+	</div>
+	
+	<!-- iPhone 8,4 -->
+	<a href="check.php?device=iPhone8,4&boardconfig=n69uap&version=11.2.5">
+	<div class="btn">
+	<center>
+	iPhone8,4, iOS 11.2.5
+	</center>
+	</div>
+	
+	<!-- iPhone 8,5 -->
+	<a href="check.php?device=iPhone9,1&boardconfig=d10ap&version=11.2.5">
+	<div class="btn_bottom">
+	<center>
+	iPhone 9,1, iOS 11.2.5 
+	</center>
+	</div>
 </div>
 </body>
 </html>
